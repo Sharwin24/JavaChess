bb93e9584236392d0f6d252f73df0df68781456b
Sharwin Patil <sharwin24@gmail.com>
2021-10-21 19:00:21 -0400

    Created a controller

---
+++ src/controller/IChessController.java
@@ -0,0 +1,12 @@
+package controller;
+
+/**
+ * An interface to represent a controller mechanism for a chess game.
+ * Offers functionality to read user input for moves and execute moves to
+ * play a chess game.
+ */
+public interface IChessController {
+
+  void playGame();
+
+}
