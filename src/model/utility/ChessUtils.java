fca0876b3f3b2db4fdf57580120ca53025b21bd2
Sharwin Patil <sharwin24@gmail.com>
2021-11-15 08:38:52 -0500

    Updated fileMap initialization

--- src/model/utility/ChessUtils.java
+++ src/model/utility/ChessUtils.java
@@ -11,19 +11,16 @@ import model.chessboard.IChessSquare;
  */
 public final class ChessUtils {
 
-  private static final HashMap<Integer, String> fileMap
-      = new HashMap<>(8);
-
-  public ChessUtils() {
-    fileMap.put(0, "a");
-    fileMap.put(1, "b");
-    fileMap.put(2, "c");
-    fileMap.put(3, "d");
-    fileMap.put(4, "e");
-    fileMap.put(5, "f");
-    fileMap.put(6, "g");
-    fileMap.put(7, "h");
-  }
+  private static final HashMap<Integer, String> fileMap = new HashMap<>(8) {{
+    put(0, "a");
+    put(1, "b");
+    put(2, "c");
+    put(3, "d");
+    put(4, "e");
+    put(5, "f");
+    put(6, "g");
+    put(7, "h");
+  }};
 
   /**
    * Gets the file letter as a string given the index. Throws an exception if the index is out of
